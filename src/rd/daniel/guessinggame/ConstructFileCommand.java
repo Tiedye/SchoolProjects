1c189fb420689773752279e204bae3bda62f6fae
Daniel <Daniel@DanielDesktop>
2015-03-31 22:26:13 -0400

    Change package name

--- src/rd/daniel/guessinggame/ConstructFileCommand.java
+++ src/rd/daniel/guessinggame/ConstructFileCommand.java
@@ -3,16 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rd.daniel;
+package rd.daniel.guessinggame;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.util.Random;
 import java.util.Scanner;
 import net.dtw.command.Command;
 import net.dtw.command.IllegalArgumentCountException;
@@ -21,13 +19,7 @@ import net.dtw.command.IllegalArgumentCountException;
  *
  * @author Daniel
  */
-public class LoadWordCommand extends Command {
-
-    private final StringBuilder word;
-
-    public LoadWordCommand(StringBuilder word) {
-        this.word = word;
-    }
+public class ConstructFileCommand extends Command {
 
     @Override
     protected void doCommand(String[] args, InputStream in, PrintStream out) throws IllegalArgumentException, IllegalArgumentCountException {
@@ -38,34 +30,39 @@ public class LoadWordCommand extends Command {
         }
 
         // check validity of filename
-        File inputFile = new File(args[0]);
+        File outputFile = new File(args[0]);
         try {
             // is it a valid windows filename
-            inputFile.getCanonicalPath();
+            outputFile.getCanonicalPath();
         } catch (IOException ex) {
             out.println("'" + args[0] + "' is not a valid filename.");
             return;
         }
-        FileInputStream inputStream;
+        PrintWriter outputWriter;
         try {
-            // is it readable
-            inputStream = new FileInputStream(inputFile);
+            // is it writeable
+            outputWriter = new PrintWriter(outputFile);
         } catch (FileNotFoundException ex) {
-            out.println("The file '" + inputFile.getPath() + "' could not be read.");
+            out.println("The file '" + outputFile.getPath() + "' could not be created/written to.");
             return;
         }
 
-        // reads the whole file, splits it into terms by the commas, and selects a random word from that list
-        Scanner input = new Scanner(inputStream);
-        if (input.hasNext()) {
-            String[] words = input.useDelimiter("\\Z").next().split(",");
-            word.delete(0, word.length());
-            word.append(words[new Random(System.currentTimeMillis()).nextInt(words.length)]);
-            out.println("Word selected.");
-            
-        } else {
-            out.println("File is empty.");
+        // construct string of comma separated values from user input
+        Scanner input = new Scanner(in);
+        out.println("Type up to 10 words to be added to the file or type stop to finish writing to the file.");
+        StringBuilder newCSV = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            // add up to ten words
+            String nextLine = input.nextLine();
+            if (nextLine.equals("stop")) {
+                break;
+            }
+            newCSV.append(nextLine).append(",");
         }
+        // write the csv to the file and close the file
+        outputWriter.print(newCSV);
+        outputWriter.close();
+        out.println("Word list '" + outputFile.getPath() + "' created.");
 
     }
 
--- src/rd/daniel/guessinggame/LoadWordCommand.java
+++ src/rd/daniel/guessinggame/LoadWordCommand.java
@@ -3,14 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rd.daniel;
+package rd.daniel.guessinggame;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.Random;
 import java.util.Scanner;
 import net.dtw.command.Command;
 import net.dtw.command.IllegalArgumentCountException;
@@ -19,7 +21,13 @@ import net.dtw.command.IllegalArgumentCountException;
  *
  * @author Daniel
  */
-public class ConstructFileCommand extends Command {
+public class LoadWordCommand extends Command {
+
+    private final StringBuilder word;
+
+    public LoadWordCommand(StringBuilder word) {
+        this.word = word;
+    }
 
     @Override
     protected void doCommand(String[] args, InputStream in, PrintStream out) throws IllegalArgumentException, IllegalArgumentCountException {
@@ -30,39 +38,34 @@ public class ConstructFileCommand extends Command {
         }
 
         // check validity of filename
-        File outputFile = new File(args[0]);
+        File inputFile = new File(args[0]);
         try {
             // is it a valid windows filename
-            outputFile.getCanonicalPath();
+            inputFile.getCanonicalPath();
         } catch (IOException ex) {
             out.println("'" + args[0] + "' is not a valid filename.");
             return;
         }
-        PrintWriter outputWriter;
+        FileInputStream inputStream;
         try {
-            // is it writeable
-            outputWriter = new PrintWriter(outputFile);
+            // is it readable
+            inputStream = new FileInputStream(inputFile);
         } catch (FileNotFoundException ex) {
-            out.println("The file '" + outputFile.getPath() + "' could not be created/written to.");
+            out.println("The file '" + inputFile.getPath() + "' could not be read.");
             return;
         }
 
-        // construct string of comma separated values from user input
-        Scanner input = new Scanner(in);
-        out.println("Type up to 10 words to be added to the file or type stop to finish writing to the file.");
-        StringBuilder newCSV = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-            // add up to ten words
-            String nextLine = input.nextLine();
-            if (nextLine.equals("stop")) {
-                break;
-            }
-            newCSV.append(nextLine).append(",");
+        // reads the whole file, splits it into terms by the commas, and selects a random word from that list
+        Scanner input = new Scanner(inputStream);
+        if (input.hasNext()) {
+            String[] words = input.useDelimiter("\\Z").next().split(",");
+            word.delete(0, word.length());
+            word.append(words[new Random(System.currentTimeMillis()).nextInt(words.length)]);
+            out.println("Word selected.");
+            
+        } else {
+            out.println("File is empty.");
         }
-        // write the csv to the file and close the file
-        outputWriter.print(newCSV);
-        outputWriter.close();
-        out.println("Word list '" + outputFile.getPath() + "' created.");
 
     }
 
