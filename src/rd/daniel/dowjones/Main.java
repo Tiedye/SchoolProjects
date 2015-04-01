bae1381cf6cd9dda7eeb541280174fdc7328e5d7
Tiedye <tiedye1@hotmail.com>
2015-03-31 22:28:06 -0400

    Add exit command instuctions

--- src/rd/daniel/dowjones/Main.java
+++ src/rd/daniel/dowjones/Main.java
@@ -18,12 +18,30 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<StockRecord> loadedCSV = new ArrayList<>();
+        ArrayList<StockRecord> loadedCSV = new ArrayList<>();        // output instructions
+        System.out.println("There are four commands available:\n"
+                + " ∟ convertcsvfile [file]\n"
+                + "    | Loads a csv database into\n"
+                + "    ∟ memory.\n"
+                + " ∟ createbinarydb [file]\n"
+                + "    | Outputs the database in memory\n"
+                + "    ∟ to a binary database file.\n"
+                + " ∟ getindexfromfile [file] [index]\n"
+                + "    | Gets the record at a certain\n"
+                + "    | index in a binary database\n"
+                + "    ∟ file.\n"
+                + " ∟ getindexfromfile [file] [index]\n"
+                + "    | Gets the record at a certain\n"
+                + "    | date in a binary database\n"
+                + "    ∟ file.\n"
+                + " ∟ exit\n"
+                + "    ∟ Exits the program.");
         CommandManager commandManager = new CommandManager(System.in, System.out);
         commandManager.registerCommand("convertcsvfile", new LoadCSVCommand(loadedCSV));
         commandManager.registerCommand("createbinarydb", new ConstuctDBCommand(loadedCSV));
         commandManager.registerCommand("getindexfromfile", new GetRecordFromIndexCommand());
         commandManager.registerCommand("getdatefromfile", new GetRecordFromDateCommand());
+        commandManager.registerCommand("exit", new ExitCommand(commandManager));
         commandManager.startInterpreting();
     }
     
