02793ea8baaf27ff756363280b946f8ddce72e13
Tiedye <tiedye1@hotmail.com>
2015-04-13 13:59:10 -0400

    Change Package

--- src/rd/daniel/couponmanager/Hasher.java
+++ src/rd/daniel/couponmanager/Hasher.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rd.daniel;
+package rd.daniel.couponmanager;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
